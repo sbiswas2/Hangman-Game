2e4e4f1583fefd5edcd925654a3d85297c9fa155
Setu Biswas <setubiswas@Setus-MacBook-Pro.local>
2017-08-12 18:51:48 -0500

    added random word selection, counting spaces, and user input to javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,34 @@
 // Assigning variables
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
 				"r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var phrase = ["champion", "chicago", "bulls", "hangtime", "airness",];
+var phrase = ["champion", "chicago", "bulls", "hangtime", "airness", "jordan"];
 var wins = 0;
 var losses = 0;
 var guesses = 9;
-// Randomly chooses a choice from the options array. This is the Computer's guess.
+
+// Randomly chooses a choice from the phrase array. This is the Computer's word for User to guess.
 var blankWord = phrase[Math.floor(Math.random() * phrase.length)];
+
+
+// Creating an empty array to fill with underscores to match number of letters in word.
+var answerSpaces = [];
+	for (var i = 0; i < blankWord.length; i++) {
+			answerSpaces[i] = "_";
+		}
+		console.log(answerSpaces);
+		var letters = blankWord.length;
+
+// Checking to see if the User's guess matches a valid letter in the alphabet.
+document.onkeyup = function(event) {
+        var userGuess = event.key;   
+	    for (var j = 0; j < alphabet.length; j++) {
+	    	if (userGuess === alphabet[j]) {
+	    		console.log(userGuess); //create a function here to write??
+	    		answerSpaces[j] = userGuess;
+	    		letters--;
+	    	}
+	    }
+	}
+
+
+
